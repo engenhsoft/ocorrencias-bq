00180a8621d45f9cc281bc245494d4b31a7d61a8
engesoft <breno.santos@sirtec.com.br>
2026-08-31 18:24:00 -0300

    Publica BTO, PG mínimo 2, novos tipos e rolagem — service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'ocorrencias-bq-';
-const CACHE_NAME = `${CACHE_PREFIX}2026.08.30.2-supervisor-photo-layout-1`;
+const CACHE_NAME = `${CACHE_PREFIX}2026.08.31.1-bto-pg2-other-scroll-1`;
 const APP_SHELL = [
   './',
   './index.html',
